0707a0464700d4c028c919b1bb9e041db4ab6bd9
Leonid Malyshev <malyshev@streamstar.com>
2017-02-24 09:43:32 +0100

    init

---
+++ Chromium2017/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Chromium2017
+	{
+	public class Program
+		{
+		private static void Main(string[] args)
+			{
+			int[] s = {171, 185, 66, 157, 129, 50, 197, 12, 147};
+			s = new[] {4, 6, 2, 1, 5};
+			//s = new[] {13, 2, 5};
+			var sol = new Solution();
+			Console.WriteLine($"=={sol.solution(s)}");
+			Console.Write($"=={sol.solutionStd(s)}");
+			Console.ReadKey();
+			}
+
+		public class Solution
+			{
+			#region test solution
+
+			public int solutionStd(int[] H)
+				{
+				long count = 0;
+				for (var i = 0; i < H.Length; i++)
+				{
+					count++; //When start=root=end
+					int mid = H[i];
+					var left = new List<int>();
+					var right = new List<int>();
+					for (var j = 0; j < i; j++) if (H[j] > mid) left.Add(H[j]);
+					for (int j = H.Length - 1; j > i; j--) if (H[j] > mid) right.Add(H[j]);
+					count += ZigZags(left.OrderBy(x=>x).ToList(), right.OrderBy(x=>x).ToList()) +
+						ZigZags(right.OrderBy(x=>x).ToList(), left.OrderBy(x=>x).ToList());
+				}
+				return (int) (count % 1000000007);
+				}
+
+
+			public long ZigZags(List<int> first, List<int> next)
+				{
+				if (first.Count == 0)
+					return 0;
+				if (next.Count == 0) return first.Count;
+				long count = 0;
+				for (int i = first.Count - 1; i >= 0; i--)
+				{
+					count++;
+					//count += ZigZags (next.Where (x => x > first[i]).ToList (), first.Where (x => x > first[i]).ToList ());
+					count += ZigZags(next.Where(x=>x > first[i]).ToList(), first.Skip(i).ToList());
+				}
+				return count;
+				}
+
+			#endregion
+
+			public int solution(int[] H)
+				{
+				long count = 0;
+				int dimension = H.Length;
+				for (int i = 0; i < dimension; i++)
+				{
+					long smallcount = 1;
+					int node = H[i];
+					//Form left and right arrays
+					int leftPtr = 0, rightPtr = 0;
+					int[] leftArray = new int[i];
+					int[] rightArray = new int[dimension - i - 1];
+					for (int j = 0; j < i; j++)
+					{
+						if (H[j] >= node) leftArray[leftPtr++] = H[j];
+					}
+					for (int j = i + 1; j < dimension; j++)
+					{
+						if (H[j] >= node) rightArray[rightPtr++] = H[j];
+					}
+					leftPtr--;
+					rightPtr--;
+					bool useLeft;
+					if (leftPtr >= 0 && rightPtr >= 0) useLeft = leftArray[leftPtr] > rightArray[rightPtr];
+					else useLeft = leftPtr > 0;
+					while (leftPtr >= 0|| rightPtr >= 0)
+					{
+						int[] array = useLeft ? leftArray : rightArray;
+						int[] otherarray = useLeft ? rightArray : leftArray;
+						smallcount++;
+						if (useLeft)
+						{
+							leftPtr--;
+							if (leftPtr <0)
+							{
+								useLeft = false;
+								continue;
+							}
+						}
+						else
+						{
+							rightPtr--;
+							if (rightPtr <0)
+							{
+								useLeft = true;
+								continue;
+							}
+						}
+						if (leftPtr<0 || rightPtr<0) continue;
+						if (array[useLeft ? leftPtr : rightPtr] < otherarray[useLeft ? rightPtr : leftPtr]) useLeft = !useLeft;
+					}
+					count = (count + smallcount) % 1000000007;
+				}
+				return (int) (count % 1000000007);
+				}
+			}
+		}
+	}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using Chromium2017;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+	{
+	[TestClass]
+	public class UnitTest1
+		{
+		[TestMethod]
+		public void TestMethod1()
+			{
+			int[] s ;
+			var rnd = new Random();
+			var sol = new Program.Solution();
+			var sw = new Stopwatch();
+			var swStd = new Stopwatch();
+			for (var i = 0; i < 5; i++)
+			{
+				var lst = new List<int>();
+				switch (i)
+				{
+					case 2:
+						s = new[] {171, 185, 66, 157, 129, 50, 197, 12, 147};
+						break;
+					case 1:
+						s = new[] {4, 6, 2, 1, 5};
+						break;
+					case 0:
+						s = new[] {13, 2, 5};
+						break;
+					default:
+						for (int j = rnd.Next(5, 20); j >= 0; j--) lst.Add(rnd.Next(200));
+						s = lst.ToArray();
+						break;
+				}
+				sw.Start();
+				int result = sol.solution(s);
+				sw.Stop();
+				swStd.Start();
+				int resultStd = sol.solutionStd(s);
+				swStd.Stop();
+				Assert.AreEqual(resultStd, result, $"Failed {i}: {ShowArray(s)}");
+				Console.WriteLine($"OK {i}: {ShowArray(s)}");
+				Console.WriteLine("---------------");
+			}
+			Console.WriteLine($"Effect: {sw.ElapsedTicks / swStd.ElapsedTicks}");
+			}
+
+		
+
+		private static string ShowArray(int[] s)
+			{
+			var str = "";
+			s.ToList().ForEach(x=>str += $" {x}");
+			return str;
+			}
+		}
+	}
